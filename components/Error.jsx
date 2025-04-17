12b46b264c6b96c3235784c1f7daae1ac957ceaf
Tushar1734 <pateltushar1734@gmail.com>
2025-04-17 11:35:24 +0530

    feat: update favicon and title in index.html; add weather app logo

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,6 +1,6 @@
 function Error({error}) {
   return (
-    <div className="text-red-500 text-lg text-center font-semibold p-3 shadow-md animate-pulse">
+    <div className="text-red-500 text-lg text-center font-semibold  p-3 shadow-md animate-pulse">
       <h2>Error</h2>
       <p>{error}</p>
     </div>
